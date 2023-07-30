b9afe919d811ba239f9a21ea9c1092aa25ad39e9
Brian Li <brian@brianli.com>
2023-07-30 15:22:01 +0900

    Update __init__.py

--- fly_python_sdk/__init__.py
+++ fly_python_sdk/__init__.py
@@ -1,13 +1,6 @@
-import logging
-
 """A Python SDK for interacting with the Fly.io API."""
 
 __version__ = "0.1"
-
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(message)s",
-)
 
 DEFAULT_API_TIMEOUT = 60
 
